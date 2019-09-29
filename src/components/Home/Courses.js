bddda1cda89996673d5d955e50683a0022f84308
John <johnsmilga@gmail.com>
2019-09-29 07:47:12 -0700

    testing strapi data

--- src/components/Home/Courses.js
+++ src/components/Home/Courses.js
@@ -1,7 +1,45 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import Image from "gatsby-image"
+const query = graphql`
+  query {
+    allStrapiCourse {
+      nodes {
+        title
+        url
+        id
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 const Courses = () => {
-  return <h1>welcome to courses section</h1>
+  const {
+    allStrapiCourse: { nodes: courses },
+  } = useStaticQuery(query)
+
+  return (
+    <div className="courses">
+      {courses.map(item => {
+        const { title, url } = item
+        const image = item.image.childImageSharp.fluid
+        return (
+          <div key={item.id} style={{ width: "200px" }}>
+            <Image fluid={image} alt="course image"></Image>
+            <h6>{title}</h6>
+            <h6>{url}</h6>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Courses
