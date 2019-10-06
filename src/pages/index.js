b851860ad061afd9ed16b8e44ccdac3c70966430
John <johnsmilga@gmail.com>
2019-10-06 16:22:04 -0700

    seo added in index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import Layout from "../components/layout"
 import HomeBanner from "../components/Home/HomeBanner"
 import Courses from "../components/Home/LatestCourses"
 import Projects from "../components/Home/Projects"
-// import SEO from "../components/SEO"
+import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
-    {/* <SEO title="Home"></SEO> */}
+    <SEO title="Home"></SEO>
     <HomeBanner></HomeBanner>
     <Courses></Courses>
     <Projects></Projects>
