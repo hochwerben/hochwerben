01bf2addf4eb5aab7fa9a757c6a89c2576be9ff9
peterpaw <pb.pawelczyk@gmail.com>
2020-04-26 15:47:05 +0200

    restyle contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,17 +4,18 @@ import styles from '../css/contactForm.module.css';
 const ContactForm = () => (
   <form className={styles.form}>
     <label htmlFor="name">
-      Name
       <input type="text" name="name" id="name" />
+      Name
     </label>
     <label htmlFor="email">
-      Email
       <input type="text" name="email" id="email" />
+      Email
     </label>
     <label htmlFor="message">
+      <textarea name="message" id="message" rows="5"></textarea>
       Nachricht
-      <textarea name="message" id="message"></textarea>
     </label>
+    <button>Senden</button>
   </form>
 );
 
