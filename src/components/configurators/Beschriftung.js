87935dd4f71995fc7be1381a5b2c86d8e328e0a2
perterpaw <pb.pawelczyk@gmail.com>
2020-05-03 10:09:35 +0200

    add beschriftung configurator

---
+++ src/components/configurators/Beschriftung.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const Beschriftung = () => {
+  const [value, setValue] = useState('seiten');
+
+  const handleChange = event => setValue(event.target.value);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert(`Sie haben ${value} ausgewählt`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Wählen Sie Ihr Produkt aus:
+        <select
+          name="beschriftung"
+          id="beschriftung"
+          value={value}
+          onChange={handleChange}
+        >
+          <option value="seiten">Seiten</option>
+          <option value="seitenUndHeck2qm">Seiten und Heck (max 2qm)</option>
+          <option value="seitenHeckUndFront3qm">
+            Seiten, Heck und Front (max 3qm)
+          </option>
+          <option value="seitenHeckUndFront6qm">
+            Seiten, Heck und Front (max 6qm)
+          </option>
+        </select>
+      </label>
+      <input type="submit" value="submit" />
+    </form>
+  );
+};
+
+export default Beschriftung;
