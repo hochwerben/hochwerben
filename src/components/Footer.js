02a5c9e5c75223346a422ffa2ad7f723a4fb6fd3
perterpaw <pb.pawelczyk@gmail.com>
2020-05-09 15:17:29 +0200

    add grid to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,65 +2,61 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { IconContext } from 'react-icons';
 import { MdPhone, MdMail } from 'react-icons/md';
-import logo from '../images/white-logo.svg';
 import styles from '../css/footer.module.css';
 import icons from '../constants/SocialLinks';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.twoColumns}>
-        <div className={styles.address}>
-          <h3>Hochwerben</h3>
-          <p>
-            Nordendstr. 82-84
-            <br />
-            64546 Mörfelden-Walldorf
-          </p>
-          <br />
-        </div>
-        <div className={styles.contactInfo}>
-          <IconContext.Provider
-            value={{
-              style: { verticalAlign: 'middle', marginRight: '1rem' },
-              size: '2em',
-            }}
-          >
-            <h3 className={styles.phone}>
+      <div className={styles.grid}>
+        <IconContext.Provider
+          value={{
+            style: { verticalAlign: 'middle', marginRight: '1rem' },
+            size: '2em',
+          }}
+        >
+          <div className={styles.phone}>
+            <h3>
               <MdPhone />
               +49 6105 9777704
             </h3>
-            <a href="mailto:info@hochwerben.de" className={styles.mail}>
+          </div>
+          <div className={styles.mail}>
+            <a href="mailto:info@hochwerben.de">
               <MdMail />
               info@hochwerben.de
             </a>
-          </IconContext.Provider>
+          </div>
+        </IconContext.Provider>
+
+        <div className={styles.address}>
+          <ul>
+            <li>Hochwerben</li>
+            <li>Nordendstr. 82-84</li>
+            <li>64546 Mörfelden-Walldorf</li>
+          </ul>
         </div>
-      </div>
-      <div className={styles.logo}>
-        <Link to="/">
-          <img src={logo} alt="Hochwerben Logo" width="100px"></img>
-        </Link>
-      </div>
-      <div className={styles.icons}>
-        {icons.map(item => {
-          return (
-            <a key={item.id} href={item.url} className={styles.icon}>
-              {item.image}
-            </a>
-          );
-        })}
-      </div>
-      <div className={styles.info}>
-        © {new Date().getFullYear()} <Link to="/">Hochwerben.de</Link> - Alle
-        Rechte vorbehalten.
-      </div>
-      <div>
-        <Link to="/impressum" className={styles.impLink}>
-          Impressum
-        </Link>
-        <Link to="/datenschutz" className={styles.impLink}>
-          Datenschutzerklärung
-        </Link>
+
+        <div className={styles.icons}>
+          {icons.map(item => {
+            return (
+              <a key={item.id} href={item.url} className={styles.icon}>
+                {item.image}
+              </a>
+            );
+          })}
+        </div>
+        <div className={styles.info}>
+          © {new Date().getFullYear()} <Link to="/">Hochwerben.de</Link> - Alle
+          Rechte vorbehalten.
+        </div>
+        <div className={styles.links}>
+          <Link to="/impressum" className={styles.impLink}>
+            Impressum
+          </Link>
+          <Link to="/datenschutz" className={styles.impLink}>
+            Datenschutzerklärung
+          </Link>
+        </div>
       </div>
     </footer>
   );
