3ebcfb157e1f7869824784f4e181ca5afd843e8f
Peter Pawelczyk <pb.pawelczyk@gmail.com>
2020-06-18 13:16:31 +0200

    clear cache

--- src/components/Leistung/CardGrid.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './cards.module.css';
-
-const CardGrid = ({ children }) => (
-  <div className={styles.grid}>{children}</div>
-);
-
-export default CardGrid;
--- src/components/Leistung/LeistungTitle.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styles from './leistungTitle.module.css';
-const LeistungTitle = ({ title, subtitle }) => {
-  return <h2 className={styles.heading}>{title}</h2>;
-};
-
-export default LeistungTitle;
